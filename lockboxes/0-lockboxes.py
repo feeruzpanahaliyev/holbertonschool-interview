3486348db171e789086bc313100591dfaf0d4bfe
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-02-29 21:59:44 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -21,13 +21,10 @@ def canUnlockAll(boxes):
 
    while newly_opened_boxes:
        next_new_boxes = []
-
        for box_index in newly_opened_boxes:
            for key in boxes[box_index]:
-               if key not in accessible_boxes and key < len(boxes) :
+               if key not in accessible_boxes and key < len(boxes):
                    accessible_boxes.append(key)  # Add newly accessible box
                    next_new_boxes.append(key)  # Track for next iteration
-                   
        newly_opened_boxes = next_new_boxes[:]
-
    return len(accessible_boxes) == len(boxes)  # True if all boxes unlocked
