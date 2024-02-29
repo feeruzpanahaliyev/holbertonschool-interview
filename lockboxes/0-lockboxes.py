53adb18b1963c07c550a5f85d1291c80dbde2ed3
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-02-29 21:20:55 +0400

    Create 0-lockboxes.py

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+'''
+Lockboxes algorithm
+'''
+
+def canUnlockAll(boxes):
+   """
+   Determines if all boxes can be unlocked using the keys found within them.
+
+   Args:
+       boxes: A list of lists, where each inner list represents the keys found in a box.
+
+   Returns:
+       True if all boxes can be opened, False otherwise.
+   """
+
+   accessible_boxes = [0]  # Start with box 0 unlocked
+   newly_opened_boxes = accessible_boxes
+
+   while newly_opened_boxes:
+       next_new_boxes = []
+       for box_index in newly_opened_boxes:
+           for key in boxes[box_index]:
+               if key not in accessible_boxes:
+                   accessible_boxes.append(key)  # Add newly accessible box
+                   next_new_boxes.append(key)  # Track for next iteration
+       newly_opened_boxes = next_new_boxes
+
+   return len(accessible_boxes) == len(boxes)  # True if all boxes unlocked
+
+# Example usage
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))  # Output: True
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))  # Output: True
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))  # Output: False
