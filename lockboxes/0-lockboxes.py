d4647a0c378c9ff76e74406b0ca5a73d58df04fb
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-02-29 21:49:46 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -22,16 +22,17 @@ def canUnlockAll(boxes):
        next_new_boxes = []
        for box_index in newly_opened_boxes:
            for key in boxes[box_index]:
-               if key not in accessible_boxes:
+               if key not in accessible_boxes and key < len(boxes) :
                    accessible_boxes.append(key)  # Add newly accessible box
                    next_new_boxes.append(key)  # Track for next iteration
        newly_opened_boxes = next_new_boxes[:]
 
    return len(accessible_boxes) == len(boxes)  # True if all boxes unlocked
 
-# Example usage
 
 '''
+
+# Example usage
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes))  # Output: True
 
@@ -40,4 +41,5 @@ print(canUnlockAll(boxes))  # Output: True
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))  # Output: False
+
 '''
