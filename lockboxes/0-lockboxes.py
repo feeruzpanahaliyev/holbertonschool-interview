7d70652069356ac67b2420306e4e59f385b0af25
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-02-29 22:10:27 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,20 +6,21 @@ Lockboxes algorithm
 
 
 def canUnlockAll(boxes):
-   """
-   Returns:
-       True if all boxes can be opened, False otherwise.
-   """
+    """
+    Returns:
+        True if all boxes can be opened, False otherwise.
+    """
 
-   accessible_boxes = [0]  # Start with box 0 unlocked
-   newly_opened_boxes = accessible_boxes
+    accessible_boxes = [0]  # Start with box 0 unlocked
+    newly_opened_boxes = accessible_boxes.copy()
 
-   while newly_opened_boxes:
-       next_new_boxes = []
-       for box_index in newly_opened_boxes:
-           for key in boxes[box_index]:
-               if key not in accessible_boxes and key < len(boxes):
-                   accessible_boxes.append(key)  # Add newly accessible box
-                   next_new_boxes.append(key)  # Track for next iteration
-       newly_opened_boxes = next_new_boxes[:]
-   return len(accessible_boxes) == len(boxes)  # True if all boxes unlocked
+    while newly_opened_boxes:
+        next_new_boxes = []
+        for box_index in newly_opened_boxes:
+            for key in boxes[box_index]:
+                if key not in accessible_boxes and key < len(boxes):
+                    accessible_boxes.append(key)  # Add newly accessible box
+                    next_new_boxes.append(key)  # Track for next iteration
+        newly_opened_boxes = next_new_boxes[:]
+
+    return len(accessible_boxes) == len(boxes)  # True if all boxes unlocked
