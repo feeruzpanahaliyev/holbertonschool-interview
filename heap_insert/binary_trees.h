117ab53a5c73277639bb319e774f0bdeb0ae5c91
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-03-15 17:18:32 +0400

    Update binary_trees.h

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
 #include <stdio.h>
@@ -16,27 +16,19 @@
  */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
-
-/**
- * binary_tree_node - Creates a binary tree node
- * @parent: Pointer to the parent node of the node to create
- * @value: The value to put in the new node
- *
- * Return: A pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
-void heapify(heap_t **root);
-void swap_nodes(heap_t **parent, heap_t **child);
-size_t binary_tree_size(const binary_tree_t *tree);
+void heapify_up(heap_t *node);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
-
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
