256297c4d9f375950a3777dfd6172e0e807b7b31
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-03-15 16:54:21 +0400

    Update binary_trees.h

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -33,11 +33,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
-void heapify_up(heap_t **heap, heap_t *node);
-size_t binary_tree_height(const binary_tree_t *tree);
-int binary_tree_is_perfect(const binary_tree_t *tree);
+void heapify(heap_t **root);
+void swap_nodes(heap_t **parent, heap_t **child);
+size_t binary_tree_size(const binary_tree_t *tree);
+
 
 #endif /* BINARY_TREES_H */
