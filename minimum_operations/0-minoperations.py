5c56cdc433e2b697a6bcb3a108b8d0e6519b295e
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-03-22 17:56:51 +0400

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
 
+"""
+Module to calc. the minimum number of operations to reach n characters of 'H'
+"""
 
-def minOperations(n):
+
+def min_operations(n):
+    """
+    Calc. the minimum number of operations needed to reach n characters of 'H'.
+    Args:
+        n (int): The target number of 'H' characters.
+    Returns:
+        int: The minimum number of operations required.
+    Raises:
+        None
+    """
     if n <= 1:
         return 0
     
     operations = 0
-    div = 2
+    divisor = 2
 
     while n > 1:
-        while n % div == 0:
-            operations += div
-            n //= div
-        div += 1
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
     return operations
 
