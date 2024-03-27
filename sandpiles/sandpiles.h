2d2f5ff21df14fdd6093ca01cd237318d54fec16
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-03-27 17:54:46 +0400

    Update sandpiles.h

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,6 +1,12 @@
-#ifndef SANDPILES_H
-#define SANDPILES_H
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
 
+#include <stddef.h>
+
+
+static void print_grid(int grid[3][3]);
+
+
+int stable(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-
-#endif /* SANDPILES_H */
+#endif /* _SANDPILES_H_ */
