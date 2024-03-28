7199467e68b84476246224a46171b3950a36fe78
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-03-28 23:51:24 +0400

    Update 0-sandpiles.c

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -11,14 +11,14 @@
  */
 int stable(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            if (grid[i][j] > 3)
-                return (0);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return (0);
 
-    return (1);
+	return (1);
 }
 /**
  * print_grid - Print 3x3 grid
@@ -27,18 +27,18 @@ int stable(int grid[3][3])
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -48,49 +48,42 @@ static void print_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j, unstable_cells = 0;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-            unstable_cells += grid1[i][j] > 3;
-        }
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+			grid2[i][j] = 0;
+		}
 
-        if (unstable_cells == 0)
-        {
-            return;
-        }
+	if (stable(grid1))
+		return;
 
-        do
-        {
-            unstable_cells = 0;
-
-            for (i = 0; i < 3; i++)
-            {
-                for (j = 0; j < 3; j++)
-                {
-                    if (grid1[i][j] > 3)
-                    {
-                        grid2[i][j] += grid1[i][j] - 4;
-                        unstable_cells +=
-                            (i > 0 && grid2[i - 1][j]++ > 3) +
-                            (i < 2 && grid2[i + 1][j]++ > 3) +
-                            (j > 0 && grid2[i][j - 1]++ > 3) +
-                            (j < 2 && grid2[i][j + 1]++ > 3);
-                    }
-                }
-            }
-
-            for (i = 0; i < 3; i++)
-            {
-                for (j = 0; j < 3; j++)
-                {
-                    grid1[i][j] += grid2[i][j];
-                    grid2[i][j] = 0;
-                }
-            }
-        } while (!stable(grid1));
-    }
+	do {
+		printf("=\n");
+		print_grid(grid1);
+		for (i = 0; i < 3; i++)
+			for (j = 0; j < 3; j++)
+				if (grid1[i][j] > 3)
+				{
+					grid2[i][j] -= 4;
+					if (i > 0)
+						grid2[i - 1][j] += 1;
+					if (i < 2)
+						grid2[i + 1][j] += 1;
+					if (j > 0)
+						grid2[i][j - 1] += 1;
+					if (j < 2)
+						grid2[i][j + 1] += 1;
+				}
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid1[i][j] += grid2[i][j];
+				grid2[i][j] = 0;
+			}
+		}
+	} while (!stable(grid1));
 }
