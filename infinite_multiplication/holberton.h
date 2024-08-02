6bd79955107e3c66837d0effe4462e866e0159a3
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-08-02 15:31:42 +0400

    Update holberton.h

--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -1,18 +1,26 @@
-#ifndef _MULTI_H_
-#define _MULTI_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#define _TRUE_ 1
-#define _FALSE_ 0
-#define MAX1 2500
-#define MAX2 2500
-
-#include <stdlib.h>
+/* INCLUDED LIBRARIES */
 #include <stdio.h>
+#include <stdlib.h>
 
+/* FUNCTION PROTOTYPES */
+/* writes single character to standard output */
 int _putchar(char c);
-void multiply(char *num_1, char *num_2);
-int is_a_number(char *a_str);
-void sum_and_print(char answer[MAX1][MAX2], int len_1, int len_2);
-int str_len(char *a);
+/* prints error message and exits with error code (\*/
+void error(void);
+/* checks if number consists of only digits and returns digit count */
+int digit_check(char *number);
+/* sets the maximum and minimum number of digits and corresponding numbers */
+void set_max_min(unsigned int digit_count1, unsigned int digit_count2,
+		 unsigned int *max_digit, unsigned int *min_digit,
+		 char **num1, char **num2, char *arg1, char *arg2);
+/* mallocs and initializes a 2D array */
+char **create_double(unsigned int row_size, unsigned int column_size);
+/* mallocs and sets final array with sum of intermediary calculations */
+char *create_final(unsigned int size, char **calculation);
+/* reallocates final array to remove any leading zeros */
+char *final_realloc(char *final, char **calculation);
 
-#endif /*_MULTI_H_*/
+#endif
